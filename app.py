682500370d77e7c15ad2ebc760a06e72dcaae0d6
snehds12 <testallaitools@gmail.com>
2025-01-04 17:31:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("CJ Bot")
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4.0-turbo"
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
